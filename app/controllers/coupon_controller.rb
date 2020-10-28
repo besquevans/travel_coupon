e8eceb6218d58e7b18ef5a2751dedca10ff1ad93
Gavin Liu <du20783@gmail.com>
2020-10-28 17:42:05 +0800

    add coupon form

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -1,4 +1,18 @@
 class CouponController < ApplicationController
   def index
   end
+
+  def create
+    @coupon = Coupon.create(coupon_params)
+
+    if @coupon.save
+    else
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :id_number, :phone)
+  end
 end
