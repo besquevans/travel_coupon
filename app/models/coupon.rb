62f7580972dd68e05c6c41e766ee4bf54d703b31
Gavin Liu <du20783@gmail.com>
2020-10-28 17:41:41 +0800

    add id number check for coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,4 +2,10 @@ class Coupon < ApplicationRecord
   validates :name, presence: true
   validates :id_number, presence: true
   validates :phone, presence: true
+
+  before_create :check_id_number
+
+  def check_id_number
+    self.id_number.match? /^[A-Z]\d{9}$/
+  end
 end
